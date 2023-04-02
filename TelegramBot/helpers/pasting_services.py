006e93880528e9809fe3fba4f638ea5bfca2f945
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-04-02 12:53:54 +0530

    Update pasting_services.py

--- TelegramBot/helpers/pasting_services.py
+++ TelegramBot/helpers/pasting_services.py
@@ -5,54 +5,37 @@ from telegraph.aio import Telegraph
 
 async def katbin_paste(text: str) -> str:
     """
-    paste the text in katb.in website.
+    Paste the given text to katb.in website and returns the url of the paste.
     """
+    
+    katbin_url = "https://katb.in/api/paste"
+    async with AsyncClient() as client:
+        try:
+            response = await client.post(katbin_url, json={"paste": {"content": "{}".format(text)}})
+            paste_id = response.json()["id"]
+            print("https://katb.in/{}".format(paste_id))
+        except Exception as e:
+            return "Something went wrong while pasting text in katb.in)
+            
+           
 
-    katbin_url = "https://katb.in"
-    client = AsyncClient()
-    response = await client.get(katbin_url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    csrf_token = soup.find("input", {"name": "_csrf_token"}).get("value")
-    try:
-        paste_post = await client.post(
-            katbin_url,
-            data={"_csrf_token": csrf_token, "paste[content]": text},
-            follow_redirects=False)
-        output_url = f"{katbin_url}{paste_post.headers['location']}"
-        await client.aclose()
-        return output_url
-    except:
-        return "something went wrong while pasting text in katb.in."
-
-
-async def telegraph_paste(content: str, title="TelegramBot") -> str:
+async def telegraph_paste(content: str, title="UsenetBot") -> str:
     """
     paste the text in telegra.ph (graph.org) website (text should follow proper html tags).
     """
 
     telegraph = Telegraph(domain="graph.org")
-    
+
     await telegraph.create_account(short_name=title)
-    html_content = "<pre>" + content.replace("\n", "<br>") + "</pre>"
+    html_content = content.replace("\n", "<br>")
     try:
-        response = await telegraph.create_page(title=title, html_content=html_content)
+        response = await telegraph.create_page(
+            title="Usenet Bot search result -", html_content=html_content)
         response = response["url"]
     except:
         response = await katbin_paste(content)
 
-    try: await telegraph.revoke_access_token()
+    try:
+        await telegraph.revoke_access_token()
     except: pass
     return response
-
-
-async def telegraph_image_paste(filepath: str) -> str:
-    """
-	paste the image in telegra.ph (graph.org) website. 
-	"""
-    telegraph = Telegraph(domain="graph.org")
-    try:
-        image_url = await telegraph.upload_file(filepath)
-        return "https://graph.org/" + image_url[0]["src"]
-    except Exception as error:
-        return "something went wrong while posting image."
-	
